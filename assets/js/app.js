00472176619121849467e531cba345203fc0d002
akrom077 <iakrom09@gmail.com>
2022-09-06 18:25:14 +0600

    js

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+var box= "Welcome!";
+alert(box);
+console.log("box");
+
+var userName = prompt("Ismingizni kriting")
+var userAge = prompt("Yoshingizn kriting!")
+var userCity = prompt("Yashash manzilingizn kriting")
+console.log("Mening ismim"+" "+userName+" "+"yoshim"+" "+userAge+" "+"yashash manzil"+" "+userCity)
+confirm ("you agree to visit the site");
+btn.addEventListener('click', () => {
+  userYear = 2022;
+      console.log(userYear - userAge)
+  })
